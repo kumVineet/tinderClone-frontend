621c0d3df4cc844b8a2555f75806a622baa14e4d
vasu.arya97@gmail.com <vasu.arya97@gmail.com>
2025-06-24 19:04:20 +0530

    added all the features for sign and sign in pages

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,1 +1,9 @@
 // All your global supportive function are here --->
+
+export function capitalizeFirstLetter(text: string): string {
+  if (!text) return '';
+  return text
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
